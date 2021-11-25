cd79fac1c59a94af4204f9c38d54dbd2cfbbc658
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-11-25 17:15:33 +0300

    gregory2

--- tribune/settings.py
+++ tribune/settings.py
@@ -102,7 +102,10 @@ DATABASES = {
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'tributariate.herokuapp.com',
+    '127.0.0.1'
+]
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
